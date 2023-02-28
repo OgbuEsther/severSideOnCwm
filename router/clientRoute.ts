08dd3213321949e620b4c87f78b3002d35b77923
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-02-28 15:39:10 +0100

    saved

--- router/clientRoute.ts
+++ router/clientRoute.ts
@@ -9,7 +9,7 @@ import { getAll, login, register } from "../controller/client/clientAuth";
 
 const clientAuthRouter = Router();
 
-clientAuthRouter.route("/signup").post(registerValidation, register);
+clientAuthRouter.route("/signup").post(register);
 clientAuthRouter.route("/login").post(loginValidation, login);
 clientAuthRouter.route("/").get(getAll);
 
