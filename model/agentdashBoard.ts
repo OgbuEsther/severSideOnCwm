e46def4c06caa25a1a676915f12c6f6412b88548
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-04 17:13:39 +0100

    saved

---
+++ model/agentdashBoard.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { clientDashBoard } from "./allInterfaces";
+
+interface client extends clientDashBoard, mongoose.Document {}
+
+export const clientSchema = new mongoose.Schema<clientDashBoard>(
+  {
+    message: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "notifications",
+      },
+    ],
+    paymentLog: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "allPayments",
+      },
+    ],
+    bills: [],
+    schedule: {
+      type: String,
+    },
+    paymentStatus: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
