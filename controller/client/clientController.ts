c0a77c45c8153f5d93fa3083f43f81f4f6f7615a
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-04 17:38:11 +0100

    saved

--- controller/client/clientController.ts
+++ controller/client/clientController.ts
@@ -7,13 +7,16 @@ import clientModel from "../../model/client/clientModel";
 //create client dashboard
 export const newClientDashboard = async (req: Request, res: Response) => {
   try {
-    const { bills, message } = req.body;
+    const { paymentLog, message, bills, schedule, paymentStatus } = req.body;
 
     const getUser = await clientModel.findById(req.params.userId);
     if (getUser) {
       const clientDashBoard = await clientDashBoardModel.create({
-        bills,
         message,
+        paymentLog,
+        bills,
+        schedule,
+        paymentStatus,
       });
     }
   } catch (error) {
