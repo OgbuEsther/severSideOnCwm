9ca7dbc30d76a0f569265551a698d8471ef2c5d6
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-04 18:41:24 +0100

    saved

--- router/ClientDashBoardRoutes.ts
+++ router/ClientDashBoardRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { newClientDashboard } from "../controller/client/clientController";
+import {
+  getAllDashboards,
+  getOneDashboard,
+  newClientDashboard,
+} from "../controller/client/clientController";
 
 const clientDashBoardRoutes = Router();
 
 clientDashBoardRoutes.post("/new/:userId", newClientDashboard);
+clientDashBoardRoutes.get("/", getAllDashboards);
+clientDashBoardRoutes.get("/:id", getOneDashboard);
 
 export default clientDashBoardRoutes;
