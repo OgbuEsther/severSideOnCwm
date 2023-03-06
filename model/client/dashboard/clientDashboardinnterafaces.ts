83ffd213f39409dff60b3d50fd5e0b0bbb6a9921
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-06 23:10:19 +0100

    saved

--- model/client/dashboard/clientDashboardinnterafaces.ts
+++ model/client/dashboard/clientDashboardinnterafaces.ts
@@ -1,11 +1,16 @@
 export interface messagedata {
   sender: string;
   date: string;
-  details: string;
+  desc: string;
 }
 
 export interface paymentLogData {
   amount: number;
   date: string;
   paymentStatus: boolean;
+}
+
+export interface contact_usData {
+  title: string;
+  detail: string;
 }
