c5fd4dba0b5c7d35d215d1e1cbc2dae97b10a821
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-07 15:03:52 +0100

    saved

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
-import express, { Application } from "express"
-import appConfig from "./app"
-import dbConfig from "./config/db"
+import express, { Application } from "express";
+import appConfig from "./app";
+import dbConfig from "./config/db";
+import adminAuthRoutes from "./router/agentRoute";
 
-const PORT : number = 8000
+const PORT: number = 8000;
 
-const app : Application = express()
-appConfig(app)
-dbConfig()
+const app: Application = express();
+appConfig(app);
+dbConfig();
 
-app.listen(PORT , () =>{
-    console.log(`server is up on port ${PORT}`)
-})
+//admin routes
+app.use("/api/adminauth", adminAuthRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is up on port ${PORT}`);
+});
