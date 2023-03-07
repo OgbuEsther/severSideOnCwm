d4c30394da7ccf51b0586c3341a798c19144b437
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-07 15:53:13 +0100

    saved changes on payment

---
+++ controller/client/AllDashboardLogics/payment.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import AdminModel from "../../../model/admin/agentmodel";
+import clientModel from "../../../model/client/clientModel";
