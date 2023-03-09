2c98d5fdd6f83b352aab7d943d028c49e262a931
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 12:28:14 +0100

    admin message dashboard

---
+++ model/admin/dashboard/adminMessage.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { adminMessage } from "./adminDashboardInterfaces";
+
+interface IAdminMessage extends adminMessage, mongoose.Document {}
+
+const adminMessageSchema = new mongoose.Schema<adminMessage>({
+  sender: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+});
