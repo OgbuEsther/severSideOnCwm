9500a8756117962c72c77959ff252afc1acb8926
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-09 12:34:49 +0100

    admin payment dashboard

---
+++ model/admin/dashboard/adminPaymentLog.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { adminPayment } from "./adminDashboardInterfaces";
+
+interface IAdminPayment extends adminPayment, mongoose.Document {}
+
+const adminPaymentSchema = new mongoose.Schema<adminPayment>({
+  amount: {
+    type: Number,
+  },
+  date: {
+    type: String,
+  },
+  viewSender: {
+    type: String,
+  },
+});
