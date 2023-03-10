99132a9f425eddda64bfac6f5d85fb4932a1fd2f
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-11 00:03:48 +0100

    saved admin dashboard

--- controller/admin/dashboard/adminMsgController.ts
+++ controller/admin/dashboard/adminMsgController.ts
@@ -15,7 +15,7 @@ export const adminToOneUser = async (
     const { sender, date, desc } = req.body;
 
     //getting admin dashboard details
-      const getDashboard = await adminDashboardModel.findById(
+    const getDashboard = await adminDashboardModel.findById(
       req.params.adminDashboard
     );
 
@@ -36,7 +36,25 @@ export const adminToOneUser = async (
         desc,
       });
 
-      await 
+      await getDashboard?.message.push(
+        new mongoose.Types.ObjectId(adminMsg?._id)
+      );
+      getDashboard?.save();
+
+      await clientModel.findByIdAndUpdate(getUser?._id, {
+        $push: { notification: adminMsg._id },
+      });
+      // await AdminModel.findByIdAndUpdate(getAdmin._id, {
+      //   $push: { notification: newMsg._id },
+      // });
+      return res.status(201).json({
+        message: "message successfully sent",
+        data: adminMsg,
+      });
+    } else {
+      return res.status(404).json({
+        message: "user or admin not found",
+      });
     }
   } catch (error) {
     return res.status(400).json({
