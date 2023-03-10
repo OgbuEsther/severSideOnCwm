4e298d9131296633bb429f134289f7d20a7c16b8
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 15:08:31 +0100

    saved admin dashboard

--- controller/admin/dashboard/adminMsgController.ts
+++ controller/admin/dashboard/adminMsgController.ts
@@ -1,1 +1,7 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
+import AdminModel from "../../../model/admin/agentmodel";
+import adminDashboardModel from "../../../model/admin/agentdashBoard";
+import adminMessageModel from "../../../model/admin/dashboard/adminMessage";
+
+//admin to user
