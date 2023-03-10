8e1476742a4c6a7999eb7c66243d3c05563416d5
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 23:43:41 +0100

    saved

--- controller/admin/dashboard/adminMsgController.ts
+++ controller/admin/dashboard/adminMsgController.ts
@@ -3,16 +3,38 @@ import mongoose from "mongoose";
 import AdminModel from "../../../model/admin/agentmodel";
 import adminDashboardModel from "../../../model/admin/agentdashBoard";
 import adminMessageModel from "../../../model/admin/dashboard/adminMessage";
+import clientModel from "../../../model/client/clientModel";
 
 //admin to user
 
-export const adminToOneUser = async (req:Request , res:Response):Promise<Response> => {
-    try {
-        
-    } catch (error) {
-      return res.status(400).json({
-        message: "bad request , unable to send message",
-        data: error,
-      });    
+export const adminToOneUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { sender, date, desc } = req.body;
+
+    //get date
+    const getDate = new Date().toDateString();
+
+    //getting the user details
+
+    const getUser = await clientModel.findById(req.params.userId);
+
+    //getting admin details
+
+    const getAdmin = await AdminModel.findById(req.params.adminId);
+    if (getAdmin && getUser) {
+      const adminMsg = await adminMessageModel.create({
+        sender: getAdmin?.name,
+        date: getDate,
+        desc,
+      });
     }
-}
+  } catch (error) {
+    return res.status(400).json({
+      message: "bad request , unable to send message",
+      data: error,
+    });
+  }
+};
