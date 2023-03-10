576a0740bbef87a7cba36da1de089c77cde5b328
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 21:59:22 +0100

    saved

--- controller/admin/dashboard/adminMsgController.ts
+++ controller/admin/dashboard/adminMsgController.ts
@@ -5,3 +5,14 @@ import adminDashboardModel from "../../../model/admin/agentdashBoard";
 import adminMessageModel from "../../../model/admin/dashboard/adminMessage";
 
 //admin to user
+
+export const adminToOneUser = async (req:Request , res:Response):Promise<Response> => {
+    try {
+        
+    } catch (error) {
+      return res.status(400).json({
+        message: "bad request , unable to send message",
+        data: error,
+      });    
+    }
+}
